3119d8296397e71500bb43e86cfb9a61bff3e00c
gt-yordanos <gt.yordanos@gmail.com>
2025-08-08 16:08:54 +0300

    added a logical order to the validation

--- routes/schoolRoutes.js
+++ routes/schoolRoutes.js
@@ -14,12 +14,24 @@ const validate = (req, res, next) => {
 
 // Validation for adding a school
 const addSchoolValidation = [
-  body('name').trim().notEmpty().withMessage('Name is required'),
-  body('address').trim().notEmpty().withMessage('Address is required'),
+  body('name')
+    .trim()
+    .notEmpty()
+    .withMessage('Name is required'),
+  body('address')
+    .trim()
+    .notEmpty()
+    .withMessage('Address is required'),
   body('latitude')
+    .exists({ checkFalsy: true })
+    .withMessage('Latitude is required')
+    .bail()
     .isFloat({ min: -90, max: 90 })
     .withMessage('Latitude must be a number between -90 and 90'),
   body('longitude')
+    .exists({ checkFalsy: true })
+    .withMessage('Longitude is required')
+    .bail()
     .isFloat({ min: -180, max: 180 })
     .withMessage('Longitude must be a number between -180 and 180'),
   validate,
@@ -30,11 +42,13 @@ const listSchoolsValidation = [
   query('latitude')
     .exists({ checkFalsy: true })
     .withMessage('Latitude query param is required')
+    .bail()
     .isFloat({ min: -90, max: 90 })
     .withMessage('Latitude must be a number between -90 and 90'),
   query('longitude')
     .exists({ checkFalsy: true })
     .withMessage('Longitude query param is required')
+    .bail()
     .isFloat({ min: -180, max: 180 })
     .withMessage('Longitude must be a number between -180 and 180'),
   validate,
