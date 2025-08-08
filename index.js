56216a418133156ea79b33adb0d96c076c13bba1
gt-yordanos <gt.yordanos@gmail.com>
2025-08-08 16:56:05 +0300

    fixed typo error

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res)=>{
-  res.send("Welcome to School Educase India Management System")
+  res.send("Welcome to Educase India School Management System")
 })
 
 app.use(bodyParser.json());
